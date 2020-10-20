06ff06e337c9442e0a338dd727fb28b9f55884e0
JdelaFuente <javidelafountain@users.noreply.github.com>
2020-10-20 13:21:08 -0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,12 @@
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
-    frase = randint(0, 8)
+    frase = randint(0, 9)
     if (frase == 0) {
-        basic.showString("LOS PULPOS TIENE 3 CORAZONES")
+        basic.showString("LOS PULPOS TIENEN 3 CORAZONES")
         basic.showIcon(IconNames.Heart)
+        basic.pause(2000)
     } else if (frase == 1) {
-        basic.showString("BUTTERFLIES CAN TASTE WITH THEIR FEETS")
+        basic.showString("BUTTERFLIES CAN TASTE WITH THEIR FEET")
     } else if (frase == 2) {
         basic.showString("LAS MOMIAS CHINCHORRO SON LAS MAS ANTIGUAS DEL MUNDO")
     } else if (frase == 3) {
@@ -17,35 +18,138 @@ input.onButtonPressed(Button.A, function () {
     } else if (frase == 6) {
         basic.showString("CUEXCOMATE: WORLD'S SMALLEST VOLCANO")
     } else if (frase == 7) {
-        basic.showString("\"CUANDO UNO ESTA ABURRIDO, DE REPENTE SE LE OCURREN IDEAS. A MI SE ME OCURRIO HOY UNA IDEA ESTUPENDA, PERO SE ME OLVIDO. OJALA MAÑANA VUELVA\" PAPELUCHO ")
+        basic.showString("\"CUANDO UNO ESTA ABURRIDO, DE REPENTE SE LE OCURREN IDEAS. A MI SE ME OCURRIO HOY UNA IDEA ESTUPENDA, PERO SE ME OLVIDO. OJALA MANANA VUELVA\" PAPELUCHO ")
     } else if (frase == 8) {
         basic.showString("THERE ARE 24 SPECIES OF DANCING FROGS")
+    } else if (frase == 9) {
+        basic.showString("TRAMPOSO")
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+        basic.pause(5000)
     }
     basic.clearScreen()
+    basic.pause(100)
 })
 input.onButtonPressed(Button.AB, function () {
     basic.clearScreen()
     input.calibrateCompass()
 })
 input.onButtonPressed(Button.B, function () {
+    brujula = input.compassHeading()
     basic.clearScreen()
-    basic.showArrow(ArrowNames.South)
+    basic.showString("EL SUR ESTA")
+    basic.clearScreen()
+    basic.showNumber(brujula)
+    basic.pause(100)
+    if (input.compassHeading() > 158 && input.compassHeading() < 203) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+        basic.pause(5000)
+    } else if (input.compassHeading() > 68 && input.compassHeading() < 113) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+        basic.pause(5000)
+    } else if (input.compassHeading() > 248 && input.compassHeading() < 293) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+        basic.pause(5000)
+    } else if (input.compassHeading() > 113 && input.compassHeading() < 158) {
+        basic.showLeds(`
+            . . . . #
+            # . . # .
+            # . # . .
+            # # . . .
+            # # # # .
+            `)
+        basic.pause(5000)
+    } else if (input.compassHeading() > 203 && input.compassHeading() < 248) {
+        basic.showLeds(`
+            # # # # .
+            # # . . .
+            # . # . .
+            # . . # .
+            . . . . #
+            `)
+        basic.pause(5000)
+    } else if (input.compassHeading() > 293 && input.compassHeading() < 338) {
+        basic.showLeds(`
+            . . . . #
+            # . . # .
+            # . # . .
+            # # . . .
+            # # # # .
+            `)
+        basic.pause(5000)
+    } else if (input.compassHeading() > 23 && input.compassHeading() < 68) {
+        basic.showLeds(`
+            # . . . .
+            . # . . #
+            . . # . #
+            . . . # #
+            . # # # #
+            `)
+        basic.pause(5000)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+        basic.pause(5000)
+    }
+    basic.pause(5000)
 })
+let brujula = 0
 let frase = 0
 frase = 0
 let caritas = 0
 basic.showString("OLITA DE MAR")
 basic.clearScreen()
 basic.forever(function () {
-    for (let index = 0; index < 2; index++) {
-        basic.showLeds(`
-            . . . # .
-            . . # # #
-            . # # . .
-            # # # # .
-            # # # # #
-            `)
-    }
+    brujula = input.compassHeading()
+    basic.showLeds(`
+        . . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        `)
+    basic.showLeds(`
+        # . . . .
+        # # . . .
+        . . . . .
+        # . . . .
+        # # # # #
+        `)
+    basic.showLeds(`
+        . # . . .
+        # # # . .
+        # . . . .
+        # # . . .
+        # # # # #
+        `)
     basic.showLeds(`
         . . # . .
         . # # # .
@@ -58,6 +162,34 @@ basic.forever(function () {
         . . # # #
         . # # . .
         # # # # .
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . # #
+        . . # # .
+        . # # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . # #
+        . . # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
         # # # # #
         `)
 })
